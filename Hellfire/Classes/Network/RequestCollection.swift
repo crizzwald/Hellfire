173ca834d44855931e94b4393c4ffc19261f45db
Ed Hellyer <ehellyer@comcast.net>
2020-07-17 13:33:44 -0400

    Added missing cancel() call on URLSessionTask()

--- Hellfire/Classes/Network/RequestCollection.swift
+++ Hellfire/Classes/Network/RequestCollection.swift
@@ -31,32 +31,34 @@ internal class RequestCollection {
     //Index of the tasks
     private var taskIndex = [RequestTaskIdentifier: TaskRequestPair]()
     
-    ///Public API
+    ///Internal API
     
-    func removeTask(forRequest request: URLRequest) {
+    internal func removeTask(forRequest request: URLRequest) {
         self.serialMessageQueue.sync {
             if let key = self.requests.removeValue(forKey: request) {
                 let _ = self.taskIndex.removeValue(forKey: key.taskIdentifier)
+                key.cancel()
             }
         }
     }
 
-    func removeRequest(forTaskIdentifier taskIdentifier: RequestTaskIdentifier) {
+    internal func removeRequest(forTaskIdentifier taskIdentifier: RequestTaskIdentifier) {
         self.serialMessageQueue.sync {
             if let key = self.taskIndex.removeValue(forKey: taskIdentifier) {
-                let _ = self.requests.removeValue(forKey: key.request)
+                let request = self.requests.removeValue(forKey: key.request)
+                request?.cancel()
             }
         }
     }
     
-    func add(request: URLRequest, task: URLSessionTask) {
+    internal func add(request: URLRequest, task: URLSessionTask) {
         self.serialMessageQueue.sync {
             let _ = self.requests.updateValue(task, forKey: request)
             let _ = self.taskIndex.updateValue((task, request), forKey: task.taskIdentifier)
         }
     }
     
-    func allTasks() -> [RequestTaskIdentifier] {
+    internal func allTasks() -> [RequestTaskIdentifier] {
         let taskIdentifiers = self.taskIndex.compactMap { $0.key }
         return taskIdentifiers
     }
